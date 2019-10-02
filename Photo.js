124460edba1781ab6b74347ab0935432378697ec
Vasyl <48532723+Pride13@users.noreply.github.com>
2019-10-02 13:12:13 +0300

    Delete Photo.js

--- Photo.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-function photoFunc(putPhoto, takePhoto) {
-
-    fs.createReadStream(takePhoto).pipe(fs.createWriteStream(putPhoto));
-    console.log('Yahoo');
-}
-
-module.exports = photoFunc;
