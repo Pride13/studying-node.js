c778cce24c3831e793534a590b24e82e757a9ef6
Vasyl <48532723+Pride13@users.noreply.github.com>
2019-10-02 10:28:12 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,12 +1,16 @@
-let fs = require('fs');
+const fs = require('fs');
 
 function students(group, name, data) {
+
     fs.mkdir(`./${group}/${name}`, err => {
+
         if (!err) {
             console.log('Ok');
         }
     });
+
     fs.writeFile(`./${group}/${name}/text.txt`, data, err => {
+
         if (!err) {
             console.log('Nice');
         }
