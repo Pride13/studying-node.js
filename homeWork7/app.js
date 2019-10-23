ff0541666d279e0b32811a333174de7c8e0bac91
Vasyl <vasilkuzyk@gmail.com>
2019-10-23 19:15:21 +0300

    new commit, chat

--- homeWork7/app.js
+++ homeWork7/app.js
@@ -3,11 +3,35 @@ const expHbs = require('express-handlebars');
 const path = require('path');
 const fileUpload = require('express-fileupload');
 
-
 const app = express();
 const dataBase = require('./dataBase').getInstance();
 dataBase.setModels();
 global.appRoot = __dirname;
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+io.on('connection', socket => {
+
+    socket.on('message', data => {
+        console.log(data);
+
+        io.emit('respMessage', {id: socket.id, data})
+    });
+
+    socket.on('msg', data => {
+        console.log(data);
+
+        io.to('2').emit('msg_resp', data)
+
+    });
+
+    socket.on('joinRoom', data => {
+        console.log(data);
+        socket.join(data.room_id);
+    });
+});
 
 app.use(fileUpload());
 app.use(express.json());
@@ -37,9 +61,12 @@ app.get('/login', renderPage.login);
 app.get('/updateUserPages', renderPage.updateUserPages);
 app.get('/apartaments', renderPage.apartaments);
 app.get('/updateFlatPages', renderPage.updateFlatPages);
+app.get('/support', (req, res) => {
+    res.render('main')
+});
 
 app.all('*', renderPage.notFound);
 
-app.listen(3000, ()=>{
+http.listen(3000, ()=>{
     console.log(3000);
 });
